13a3becb8c1cb6882a2a7fc2290b7a3b7e42ead8
Bartłomiej Wnuk <bartekwnuk13@gmail.com>
2018-04-28 20:33:58 +0200

    Choice of start and end doesn't work.

--- interface.py
+++ interface.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from labyrinth import *
 
 # *** Button size ***
 SIZE_X = 2
@@ -20,7 +20,7 @@ class TopInterface:
 
         self.label_confirm = Label(self.topFrame, text="Juz zrobiono!", fg="red")
         self.label_error_value = Label(self.topFrame, text="Musisz podac wartosc!", fg="red")
-
+        self.label_start = Label(self.topFrame, text="Zaznacz poczatek i koniec", fg="blue")
         self.label_N = Label(self.topFrame, text="Rozmiar N:")
         self.label_M = Label(self.topFrame, text="Rozmiar M:")
 
@@ -33,12 +33,12 @@ class TopInterface:
         self.entry_N.grid(row=0, column=1)
         self.entry_M.grid(row=1, column=1)
 
-        self.button_confirm = Button(self.topFrame, text="Zatwierdz", fg="blue", command=lambda:self.get_entry(self.topFrame))
+        self.button_confirm = Button(self.topFrame, text="Zatwierdz", fg="blue", command=lambda: self.get_entry(self.topFrame))
         self.button_confirm.bind("<Button-1>")
         self.button_confirm.grid(row=2, column=1, sticky=E)
 
     def get_entry(self, master):
-        try:
+        #try:
             self.size_N = int(self.entry_N.get())
             self.size_M = int(self.entry_M.get())
 
@@ -52,7 +52,7 @@ class TopInterface:
                     self.create(master)
                     self.label_error_value.grid_forget()
                     self.counter = True
-        except:
+        #except:
             if self.counter:
                 pass
             else:
@@ -63,51 +63,12 @@ class TopInterface:
 
     def create(self, master):
         L = Labyrinth(self.size_N, self.size_M, master)
-        L.wypisz()
+        #L.wypisz()
         L.rysuj()
+        self.create_start(L)
 
 
-# *** Labyrinth ***
-# @param1, @param2 size of Labyrinth, @param3 root
-
-class Labyrinth:
-    def __init__(self, X, Y, master):
-        self.X = X
-        self.Y = Y
-        self.L = [[Element(master, "1") for w in range(int(X))] for r in range(int(Y))]
-
-    #Test Funcition
-    def wypisz(self):
-        print(self.L[0][0].get_value())
-        for i in range(int(self.Y)):
-            for j in range(int(self.X)):
-                print(self.L[i][j].get_value())
-
-    def rysuj(self):
-        for i in range(int(self.Y)):
-            for j in range(int(self.X)):
-                self.L[i][j].get_button().grid(row=i+4, column=2+j, sticky=W)
-
-
-# *** Labyrinth's element ***
-# @param1. @param2
-
-class Element:
-    def __init__(self, master, textB):
-        self.value = 0
-        self.button = Button(master, text=textB, fg="white", bg="green")
-
-    def change_color(self, color):
-        self.button.config(fg=color)
-
-    def change_value(self, value):
-        self.value = value
-
-    def change_text(self, textC):
-        self.button.config(text=textC)
-
-    def get_button(self):
-        return self.button
-
-    def get_value(self):
-        return self.value
+    def create_start(self, L):
+        self.label_start.grid(row=4+self.size_M, column=0, sticky=W)
+        while not(L.set_start()):
+            pass
