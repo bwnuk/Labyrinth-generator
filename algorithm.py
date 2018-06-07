f4e1635ef44472d6ab94fe81e494fdade12cec03
bwnuk <bartekwnuk13@gmail.com>
2018-06-07 22:35:32 +0200

    Bot imporing #1

--- algorithm.py
+++ algorithm.py
@@ -9,29 +9,113 @@ def Bot(L, x_p, y_p, R):
     """
     Function responsible for the transition of Labyrinth
 
-    0-left, 1-down, 2-right, 3-up
+    Directions:
+    0-down, 1-right, 2-up, 3-left
 
     :param L: object Labyrinth
     :param x_p: X Start
     :param y_p: Y Start
+    :param R: road
     :return: Is true when passed
     """
     i = x_p
     j = y_p
 
-    # secure
+    ni = x_p
+    nj = y_p
 
+    d = 0
+
+    # secure
+    Rozd = []
     counter = 0
+    s = 0
+    steps = 0
 
     side = 0
 
     while L[i][j] != 3:
         try:
-            R.append([i, j])
+
+            # dol
+            if L[i + 1][j] == 1:
+                if d == 2:
+                    if L[i][j + 1] == 1:
+                        if d == 3:
+                            break
+                        if d == -1:
+                            d = 1
+                            nj = j + 1
+                        s = s + 1
+                    else:
+                        d = -1
+                    if L[i][j - 1] == 1:
+                        if d == 1:
+                            break
+                        if d == -1:
+                            d = 3
+                            nj = j - 1
+                        s = s + 1
+                    else:
+                        pass
+                d = 0
+                s = s + 1
+                ni = i + 1
+            else:
+                d = -1
+
+            # prawo
+            if L[i][j + 1] == 1:
+                if d == 3:
+                    if L[i - 1][j] == 1:
+                        if d == 0:
+                            pass
+                        if d == -1:
+                            d = 2
+                            ni = i - 1
+                        s = s + 1
+                    else:
+                        pass
+                if d == -1:
+                    d = 1
+                    nj = j + 1
+                s = s + 1
+            else:
+                d = -1
+
+            # gora
+            if L[i - 1][j] == 1:
+                if d == 0:
+                    pass
+                if d == -1:
+                    d = 2
+                    ni = i - 1
+                s = s + 1
+            else:
+                d = -1
+
+            #lewo
+            if L[i][j - 1] == 1:
+                if d == 1:
+                    pass
+                if d == -1:
+                    d = 3
+                    nj = j - 1
+                s = s + 1
+
+
+            R.append([[i, j], d])
+
+            i = ni
+            j = nj
+
+            R.append([[i, j], d])
             if side == 0:
                 # dol
                 if L[i + 1][j] == 1 or L[i + 1][j] == 3 or L[i + 1][j] == 4:
                     i = i + 1
+                    R.append([[i, j], d])
+                    steps = steps + 1
                 # lewo
                 elif L[i][j - 1] == 1 or L[i][j - 1] == 3 or L[i][j - 1] == 4:
                     j = j - 1
@@ -69,14 +153,13 @@ def Bot(L, x_p, y_p, R):
         if counter >= 1000000:
             print("ZA DUZO")
             return False
-    print(L[i][j], i, j)
-    print(R)
+    #print(L[i][j], i, j)
+    #print(R)
     return True
 
 
 # *** Generator ***
-# List comprehension, algorithm
-# Hunt and Kill
+# List comprehension, algorith
 
 def generator(my, mx):
     """
@@ -132,3 +215,52 @@ def pisz(L, r, k):
     print(" ")
 
 # Values 0-wall, 1-road, 2-entrance, 3-exit, 4-middle
+
+
+
+            if side == 0:
+                # dol
+                if L[i + 1][j] == 1 or L[i + 1][j] == 3 or L[i + 1][j] == 4:
+                    i = i + 1
+                    R.append([[i, j], d])
+                    steps = steps + 1
+                # lewo
+                elif L[i][j - 1] == 1 or L[i][j - 1] == 3 or L[i][j - 1] == 4:
+                    j = j - 1
+                # prawo
+                elif L[i][j + 1] == 1 or L[i][j + 1] == 3 or L[i][j + 1] == 4:
+                    side = 1
+                # gora
+                elif L[i - 1][j] == 1 or L[i - 1][j] == 3 or L[i - 1][j] == 4:
+                    i = i - 1
+            elif side == 1:
+                if L[i][j + 1] == 1 or L[i][j + 1] == 3 or L[i][j + 1] == 4:
+                    j = j + 1
+                elif L[i + 1][j] == 1 or L[i + 1][j] == 3 or L[i + 1][j] == 4:
+                    side = 0
+                elif L[i - 1][j] == 1 or L[i - 1][j] == 3 or L[i - 1][j] == 4:
+                    side = 2
+                elif L[i][j - 1] == 1 or L[i][j - 1] == 3 or L[i][j - 1] == 4:
+                    j = j - 1
+            elif side == 2:
+                if L[i - 1][j] == 1 or L[i - 1][j] == 3 or L[i - 1][j] == 4:
+                    i = i - 1
+                elif L[i][j + 1] == 1 or L[i][j + 1] == 3 or L[i][j + 1] == 4:
+                    j = j + 1
+                    side = 1
+                elif L[i][j - 1] == 1 or L[i][j - 1] == 3 or L[i][j - 1] == 4:
+                    j = j - 1
+        except:
+            if side != 2:
+                side = side + 1
+            else:
+                side = 0
+
+        counter = counter + 1
+
+        if counter >= 1000000:
+            print("ZA DUZO")
+            return False
+    #print(L[i][j], i, j)
+    #print(R)
+    return True
